c160e39fd19d4bdabbbbcd532cc0afd0776359b1
César "kaiser" Campos <112444907+riqcampos@users.noreply.github.com>
2022-09-25 17:51:00 -0300

    MainActivity.java

---
+++ Atv3-CalcAreaSlider/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.br.calcarearange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calc();
+    }
+
+    public void calc() {
+        final SeekBar s_width = (SeekBar) findViewById(R.id.w_slide);
+        final SeekBar s_height = (SeekBar) findViewById(R.id.h_slide);
+        final TextView area_view = (TextView) findViewById(R.id.area_text);
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                int width = (int) s_width.getProgress();
+                int height = (int) s_height.getProgress();
+                area_view.setText("Área = " + width*height + "u²");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+        s_height.setOnSeekBarChangeListener(listener);
+        s_width.setOnSeekBarChangeListener(listener);
+    }
+
+
+}
